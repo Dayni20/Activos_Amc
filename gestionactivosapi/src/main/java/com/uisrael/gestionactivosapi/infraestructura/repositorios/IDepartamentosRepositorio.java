77146e42a45dd7ddbe4cff3315257269e4b7adc2
elizabethgovi <elizabethgovi@gmail.com>
2025-12-17 13:36:33 -0500

    Se agregan repositorios JPA de categorias, departamentos, roles, ubicaciones y usuarios

---
+++ gestionactivosapi/src/main/java/com/uisrael/gestionactivosapi/infraestructura/repositorios/IDepartamentosRepositorio.java
@@ -0,0 +1,10 @@
+package com.uisrael.gestionactivosapi.infraestructura.repositorios;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.uisrael.gestionactivosapi.infraestructura.persistencia.jpa.DepartamentosJpa;
+
+public interface IDepartamentosRepositorio
+        extends JpaRepository<DepartamentosJpa, Integer> {
+
+}
---
+++ gestionactivosapi/src/main/java/com/uisrael/gestionactivosapi/infraestructura/repositorios/IUsuariosRepositorio.java
@@ -0,0 +1,10 @@
+package com.uisrael.gestionactivosapi.infraestructura.repositorios;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.uisrael.gestionactivosapi.infraestructura.persistencia.jpa.UsuariosJpa;
+
+public interface IUsuariosRepositorio
+        extends JpaRepository<UsuariosJpa, Integer> {
+
+}
