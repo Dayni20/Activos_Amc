0a1d2d767b725390458c290da6d153ac1a65aaf3
Dayni20 <dayniliss@hotmail.com>
2025-12-16 20:30:17 -0500

    Añadir capas con nueva configuracion

---
+++ gestionactivosapi/src/main/java/com/uisrael/gestionactivosapi/GestionactivosapiApplication.java
@@ -0,0 +1,13 @@
+package com.uisrael.gestionactivosapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionactivosapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionactivosapiApplication.class, args);
+	}
+
+}
---
+++ gestionactivosapi/src/test/java/com/uisrael/gestionactivosapi/GestionactivosapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uisrael.gestionactivosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionactivosapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
