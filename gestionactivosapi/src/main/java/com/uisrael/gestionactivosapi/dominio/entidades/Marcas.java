14f56fd8680d24d6a65e688be41c07f579079400
Alexander Tupiza <atupiza@amcecuador.com>
2025-12-17 10:35:32 -0500

    Logica de las clases

--- gestionactivosapi/src/main/java/com/uisrael/gestionactivosapi/dominio/entidades/Marcas.java
+++ gestionactivosapi/src/main/java/com/uisrael/gestionactivosapi/dominio/entidades/Marcas.java
@@ -1,5 +1,35 @@
 package com.uisrael.gestionactivosapi.dominio.entidades;
 
-public class Marcas {
+import java.io.Serializable;
 
+public class Marcas implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private final int id_marca;
+    private final String nombre;
+    private final boolean estado;
+
+    private Marcas(int id_marca, String nombre, boolean estado) {
+        this.id_marca = id_marca;
+        this.nombre = nombre;
+        this.estado = estado;
+    }
+
+    public int getId_marca() {
+        return id_marca;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    @Override
+    public String toString() {
+        return "Marcas [id_marca=" + id_marca + ", nombre=" + nombre + ", estado=" + estado + "]";
+    }
 }
